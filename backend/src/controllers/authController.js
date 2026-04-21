be07be6237c306f674fd44dd0e38622e62106724
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-04-21 18:37:31 +0100

    Cookie And Navbar Adjustment

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export async function registerUser(req, res){
     try {
@@ -66,8 +68,8 @@ export async function loginUser(req, res){
 
         res.cookie("token", token, {
             httpOnly: true,
-            secure: true,
-            sameSite: "none",
+            secure: isProduction,
+            sameSite: isProduction ? "none" : "lax",
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -96,8 +98,8 @@ export async function getCurrentUser(req, res){
 export async function logoutUser(req, res){
     res.cookie("token", "", {
         httpOnly: true,
-        secure: true,
-        sameSite: "none",
+        secure: isProduction,
+        sameSite: isProduction ? "none" : "lax",
         expires: new Date(0),
     });
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,12 +5,14 @@ import { previewAnime } from '../api/backend';
 import { useAuth } from '../context/AuthContext';
 import {MagnifyingGlass, List} from 'phosphor-react';
 import toast from 'react-hot-toast';
+import { set } from 'mongoose';
 
 function Navbar(){
     const { user, logout } = useAuth();
     const [searchOpen, setSearchOpen] = useState(false);
     const [query, setQuery] = useState("");
     const [preview, setPreview] = useState([]);
+    const [previewLoading, setPreviewLoading] = useState(false);
     const [activeIndex, setActiveIndex] = useState(-1);
     const debounceQuery = useDebounce(query);
     const [menuOpen, setMenuOpen] = useState(false);
@@ -22,9 +24,11 @@ function Navbar(){
         async function loadPreview(){
             if(!debounceQuery){
                 setPreview([]);
+                setPreviewLoading(false);
                 return;
             }
 
+            setPreviewLoading(true);
             const results = await previewAnime(debounceQuery);
             setPreview(results);
         }
@@ -83,9 +87,12 @@ function Navbar(){
     }
     
     return(
-        <nav className='sticky top-0 z-50 bg-bgLight shadow-md
-                        px-4 lg:px-8 py-3 flex justify-between
-                        items-center'>
+        <nav className='sticky top-0 z-50
+                        bg-bgLight shadow-md
+                        px-4 lg:px-8 py-3 
+                        flex flex-col md:flex-row
+                        md:justify-between md:items-center 
+                        gap-3 md:gap-0'>
 
             <Link 
                 to='/'
@@ -97,12 +104,12 @@ function Navbar(){
             <div className='flex items-center gap-3 relative'>
                 <div 
                     ref={searchRef}
-                    className='relative flex items-center'>
+                    className='relative flex items-center w-full md:w-auto'>
                     <button
                         onClick={() => {
                             setSearchOpen(true);
                             setTimeout(() => inputRef.current?.focus(), 0)}}
-                        className='text-cyan p-1 hover:scale-110 transition'>
+                        className='text-cyan p-1 active:scale-95 transition'>
                         <MagnifyingGlass size={20} weight='bold'/>
                     </button>
                     <input
@@ -117,14 +124,26 @@ function Navbar(){
                         className={`bg-transparent border-b-2 border-cyan outline-none
                                     text-sm text-textMain ml-2 transition-all duration-200
                                     ${searchOpen ? 
-                                        "w-48 opacity-100 pointer-events-auto border-cyan" : 
+                                        "w-full md:w-48 opacity-100 pointer-events-auto border-cyan" : 
                                         "w-0 opacity-0 pointer-events-none"}`} />
 
-                    {searchOpen && preview.length > 0 && (
+                    {searchOpen && (previewLoading || preview.length > 0) && (
                         <div className='absolute top-12 right-0 w-96 bg-bgLight
                                         shadow-2xl overflow-hidden z-50
                                         border border-white/10'>
-                            {preview.map((anime, index)=> (
+                        <div className='divide-y divide-white/5'>
+                            {previewLoading ? (
+                                Array.from({ length: 4 }).map((_, i) => (
+                                    <div className='flex gap-3 px-4 py-3 animate-pulse'>
+                                        <div className='w-12 h-16 bg-white/10 rounded-sm'/>
+                                        <div className='flex flex-col gap-2 flex-1'>
+                                            <div className='h-3 bg-white/10 rounded w-3/4'></div>
+                                            <div className='h-3 bg-white/10 rounded w-1/2'></div>
+                                        </div>
+                                    </div>
+                                ))
+                            ) : (
+                            preview.map((anime, index) => (
                                 <div 
                                     key={anime.id}
                                     onMouseEnter={() => setActiveIndex(index)}
@@ -148,8 +167,15 @@ function Navbar(){
                                     </span>
                                 </div>
                                 </div>
-                            ))}
+                            )))}
+
+                            {previewLoading && preview.length === 0 && debounceQuery && (
+                                <div className='px-4 py-3 text-sm text-textMuted'>
+                                    No Results Found
+                                </div>
+                            )}
                         </div>
+                    </div>
                     )}
                 </div>
 
@@ -197,23 +223,29 @@ function Navbar(){
                         <List size={22} weight='bold'/>
                     </button>
                     {menuOpen && (
-                        <div className='absolute right-0 mt-2 w-44 bg-bgLight
+                        <div className='absolute right-0 md:right-0 left-0 md:left-auto
+                                        mt-2 w-full md:w-44 bg-bgLight
                                         border border-white/10 shadow-xl overflow-hidden
                                         z-50 flex flex-col'>
                             <Link 
                                 to='/'
+                                onClick={() => setMenuOpen(false)}
                                 className='block px-4 py-2 text-sm hover:bg-white/5 transition'>
                                 Home
                             </Link>
                             <Link 
                                 to='/watchlist'
+                                onClick={() => setMenuOpen(false)}
                                 className='block px-4 py-2 text-sm hover:bg-white/5 transition'>
                                 Watchlist
                             </Link>
                             <div className='md:hidden border-t border-white/10 mt-1'>
                                 {user ? (
                                     <button 
-                                        onClick={handleLogout}
+                                        onClick={() => {
+                                            handleLogout
+                                            setMenuOpen(false);
+                                        }}                                        
                                         className='w-full text-left px-4 py-2 text-sm hover:bg-white/5 transition'>
                                             Logout
                                         </button>
@@ -221,11 +253,13 @@ function Navbar(){
                                     <>
                                         <Link 
                                             to="/login"
+                                            onClick={() => setMenuOpen(false)}
                                             className='block px-4 py-2 text-sm hover:bg-white/5 transition'>
                                                 Login
                                         </Link>
                                         <Link
                                             to="/register"
+                                            onClick={() => setMenuOpen(false)}
                                             className='block px-4 py-2 text-sm hover:bg-white/5 transition'>
                                                 Register
                                         </Link>

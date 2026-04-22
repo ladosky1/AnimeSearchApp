d3206f71279c7638bda63bbeee401a6437fcea47
oladojabasit29@gmail.com <oladojabasit29@gmail.com>
2026-04-22 07:04:20 +0100

    Deployment ajustments

--- frontend/src/api/backend.js
+++ frontend/src/api/backend.js
@@ -1,4 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_URL;
+const API_BASE = import.meta.env.VITE_API_URL || "https://animelad.onrender.com/api";
 
 export async function getCategories(){
     const response = await fetch(`${API_BASE}/anime/categories`);
